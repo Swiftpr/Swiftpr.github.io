b37ff8ec4d820441c4822b3779a5d9d415cb4504
Paul Swift <paulswift@Pauls-MacBook.local>
2016-12-14 09:47:41 -0500

    routing

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
-//app.js
-console.log('wired up!')
-//j-query
-console.log($)
-//underscore
-console.log(_)
-//backbone
-console.log(Backbone)
 
+
+var appContainer = document.querySelector("#app-container")
+
+var AppRouter = Backbone.Router.extend({
+   routes: {
+      "contact" : "showContact",
+      "works" : "showWorks",
+      "*path": "showHome"
+
+   },
+
+   showHome: function(){
+      appContainer.innerHTML = "<h1>Welcome Home</h1>"
+   },
+
+   showWorks: function(){
+      appContainer.innerHTML = "<h2>Works Page</h2>"
+   },
+
+   showContact: function(){
+      appContainer.innerHTML = "<h2>Contact Page</h2>"
+
+   },
+
+   initialize: function(){
+
+      Backbone.history.start();
+   }
+})
+
+var myApp = new AppRouter()
